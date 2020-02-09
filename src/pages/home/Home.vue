d470380020fafec75d801c383f9678316b9adff9
chenqixian <chenqixian@buaa.edu.cn>
2020-02-09 15:52:18 +0800

    add swiper

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -2,16 +2,19 @@
 <!-- 注意：template只能对外暴露1个元素，因此最外层需要一个div包裹 -->
   <div>
     <home-header></home-header>
+    <home-swiper></home-swiper>
   </div>
 </template>
 
 <script>
 // export default允许import该模块使使用任何名称
 import HomeHeader from './components/Header'
+import HomeSwiper from './components/Swiper'
 export default {
   name: 'home',
   components: {
-    HomeHeader
+    HomeHeader,
+    HomeSwiper
   }
 }
 </script>
