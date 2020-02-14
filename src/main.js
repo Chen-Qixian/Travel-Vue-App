3542b9c929ceb41692facb8b3f3f9c55c0bc6add
chenqixian <chenqixian@buaa.edu.cn>
2020-02-14 11:14:31 +0800

    vuex change city

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App'
 import router from './router'
 import fastClick from 'fastclick'
 import VueAwesomeSwiper from 'vue-awesome-swiper'
+import store from './store'
 import 'styles/reset.css'// 解决不同浏览器样式统一适配问题
 import 'styles/border.css'// 解决1px边框在2dpr和3dpr上像素重叠问题
 import 'styles/iconfont.css'
@@ -18,6 +19,7 @@ Vue.use(VueAwesomeSwiper)
 new Vue({
   el: '#app', // 接管index.html中id为app的dom
   router, // 获取路由，ES6写法等价于{router: router}
+  store, // 获取vuex存储内容
   components: { App }, // 应用组件 {App: App}
   template: '<App/>'
 })
